53005c7f0d1833094633549110a3311e491918b9
kiran2s <kiranssivakumar@gmail.com>
2017-06-22 11:31:08 -0700

    Added results from learning iteration 2.

---
+++ printBestResultsItr2-200.bat
@@ -0,0 +1,9 @@
+python generateNextItrSignals.py Iteration2/yvals-0.0-1.6-iteration2-Signals-200-0.txt
+python generateNextItrSignals.py Iteration2/yvals-0.0-1.6-iteration2-Signals-200-1.txt
+python generateNextItrSignals.py Iteration2/yvals-0.0-1.6-iteration2-Signals-200-2.txt
+python generateNextItrSignals.py Iteration2/yvals-0.0-1.6-iteration2-Signals-200-3.txt
+
+python generateNextItrSignals.py Iteration2/yvals-0.0-1.8-iteration2-Signals-200-0.txt
+python generateNextItrSignals.py Iteration2/yvals-0.0-1.8-iteration2-Signals-200-1.txt
+python generateNextItrSignals.py Iteration2/yvals-0.0-1.8-iteration2-Signals-200-2.txt
+python generateNextItrSignals.py Iteration2/yvals-0.0-1.8-iteration2-Signals-200-3.txt
