5f452f0efba2e149fb8afbeb73c5d52105bfb408
kiran2s <kiranssivakumar@gmail.com>
2017-06-22 15:38:25 -0700

    Added data to redo iteration 3 with softer surfaces.

---
+++ printBestResultsItr2-redoSoft.bat
@@ -0,0 +1,9 @@
+python generateNextItrSignals.py Iteration2-redoSoft/yvals-0.0-1.8-iteration2-Signals-100-0.txt
+python generateNextItrSignals.py Iteration2-redoSoft/yvals-0.0-1.8-iteration2-Signals-100-1.txt
+python generateNextItrSignals.py Iteration2-redoSoft/yvals-0.0-1.8-iteration2-Signals-100-2.txt
+python generateNextItrSignals.py Iteration2-redoSoft/yvals-0.0-1.8-iteration2-Signals-100-3.txt
+
+python generateNextItrSignals.py Iteration2-redoSoft/yvals-0.0-2.0-iteration2-Signals-100-0.txt
+python generateNextItrSignals.py Iteration2-redoSoft/yvals-0.0-2.0-iteration2-Signals-100-1.txt
+python generateNextItrSignals.py Iteration2-redoSoft/yvals-0.0-2.0-iteration2-Signals-100-2.txt
+python generateNextItrSignals.py Iteration2-redoSoft/yvals-0.0-2.0-iteration2-Signals-100-3.txt
