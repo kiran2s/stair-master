27aed2372a7a1fe96e92a35c8803a70641a726aa
kiran2s <kiranssivakumar@gmail.com>
2017-07-22 10:40:24 -0700

    removed iteration batch scripts

--- printBestResultsItr2-200.bat
+++
@@ -1,9 +0,0 @@
-python generateNextItrSignals.py Iteration2/yvals-0.0-1.6-iteration2-Signals-200-0.txt
-python generateNextItrSignals.py Iteration2/yvals-0.0-1.6-iteration2-Signals-200-1.txt
-python generateNextItrSignals.py Iteration2/yvals-0.0-1.6-iteration2-Signals-200-2.txt
-python generateNextItrSignals.py Iteration2/yvals-0.0-1.6-iteration2-Signals-200-3.txt
-
-python generateNextItrSignals.py Iteration2/yvals-0.0-1.8-iteration2-Signals-200-0.txt
-python generateNextItrSignals.py Iteration2/yvals-0.0-1.8-iteration2-Signals-200-1.txt
-python generateNextItrSignals.py Iteration2/yvals-0.0-1.8-iteration2-Signals-200-2.txt
-python generateNextItrSignals.py Iteration2/yvals-0.0-1.8-iteration2-Signals-200-3.txt
